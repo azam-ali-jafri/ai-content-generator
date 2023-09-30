5346506522d9739aba4bb4a381fd66a98758cabf
Azam Ali <azamalijafrii@gmail.com>
2023-09-30 09:16:42 +0530

    Clerk Authentication

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/Button";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div>
+        <Link href="sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
