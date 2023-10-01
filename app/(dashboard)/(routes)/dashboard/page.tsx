494f4464eadff20c365f0ba07419e631352f7682
Azam Ali <azamalijafrii@gmail.com>
2023-10-01 12:13:59 +0530

    Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Dashboard = () => {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
